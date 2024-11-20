79efb671df530b6c4cec0ddc7649b27dee87bd12
Shahbano1210 <saurangz@calpoly.edu>
2024-11-20 08:32:30 -0800

    task 2

---
+++ hw4.py
@@ -0,0 +1,17 @@
+
+#task 1
+#program
+
+# load data
+print(#number of entries))
+#3xecute operations and print where appropriate
+
+#one operation per line, components separated by :
+#float only
+
+#to process the operations file,
+# LINE IS valid if blank
+# if line is malformed (missing field of data cant be converted), print an error message to the terminal
+# that states line number and continue processing
+
+
